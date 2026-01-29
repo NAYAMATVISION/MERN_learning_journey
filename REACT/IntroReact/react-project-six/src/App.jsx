ff01d137446d22bbddc92ce5786841764f8e4d6a
NAYAMATVISION <nayamatemeet@gmail.com>
2026-01-29 13:04:18 +0530

    used conditional rendering

---
+++ REACT/IntroReact/react-project-six/src/App.jsx
@@ -0,0 +1,23 @@
+
+import { useState } from 'react'
+import './App.css'
+import LogoutBtn from './components/LogoutBtn';
+import LoginBtn from './components/LoginBtn';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+ 
+   if(isLoggedIn){
+    return (
+      <LogoutBtn/>
+    )
+   }
+    else{
+      return (
+        <LoginBtn/>
+      )
+    }
+
+}
+
+export default App
